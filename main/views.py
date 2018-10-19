a442a1c89a31973ae0ddecbbed4677b8e1f8073f
Ryan Gonzales <helloryantg@gmail.com>
2018-10-19 14:59:45 -0700

    fix delete

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Widget
+from .forms import WidgetForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = '/'
+
+def index(request):
+    model = Widget
+    widget_list = Widget.objects.all()
+    form = WidgetForm()
+    return render(request, 'index.html', { 'widget_list': widget_list, 'form': form })
+
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    form.save()
+    return redirect('index')
+
+def delete(request, id):
+    widget = Widget.objects.get(id=id)
+    widget.delete()
+    return redirect('/')
